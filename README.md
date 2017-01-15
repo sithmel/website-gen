3485cb954a704056a17bbb7008519ae4575ac218
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-01-15 21:00:07 +0000

    main structure

---
+++ README.md
@@ -0,0 +1,11 @@
+website generator
+-----------------
+
+Commands:
+* npm run build: build the template
+* npm run watch: build the template, watch and rebuild on change
+
+Paths
+* Configuration: **config/data.json**
+* Contents: **templates**
+* Partials: **partials**
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const handlebars = require('gulp-compile-handlebars');
+const data = require('./config/data');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+const path = {
+  templates: 'templates/**/*',
+  partials: 'partials',
+  scss: 'scss/*.scss'
+};
+
+const dest = 'docs';
+
+const handlebar_options = {
+  batch: [path.partials],
+  helpers : {
+    capitals : function(str){
+      return str.toUpperCase();
+    }
+  }
+};
+
+/*
+* tasks
+*/
+
+gulp.task('scss', () => gulp.src(path.scss)
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest(dest)));
+
+gulp.task('html', () => gulp.src(path.templates)
+    .pipe(handlebars(data, handlebar_options))
+    .pipe(gulp.dest(dest)));
+
+
+gulp.task('default', [
+  'html',
+  'scss'
+]);
+
+gulp.task('watch', () => gulp.watch(Object.values(path), [
+  'default'
+]));
